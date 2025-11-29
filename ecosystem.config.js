c7d6558d26a196171a272e4b805804bbe101ceac
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2025-11-29 13:04:49 +0100

    PUSH - use next

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'repo-save-editor',
-      script: 'pnpm',
+      script: 'node_modules/.bin/next',
       args: 'start',
       instances: 1,
       exec_mode: 'fork',
